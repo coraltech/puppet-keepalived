35385234c523d236823653c1a95c8a47799733f6
Adrian Webb <adrian.webb@venturiscm.com>
2012-08-09 21:25:20 -0400

    Changing the build definition to make, adding some build steps, and changing some requirements.

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,23 +25,30 @@
 # [Remember: No empty lines between comments and class definition]
 class keepalived (
 
-  $repo                   = $keepalived::params::os_keepalived_repo,
-  $source                 = $keepalived::params::keepalived_source,
-  $revision               = $keepalived::params::keepalived_revision,
-  $service                = $keepalived::params::os_keepalived_service,
-  $service_ensure         = $keepalived::params::keepalived_service_ensure,
-  $firewall_ports         = $keepalived::params::firewall_ports,
-  $config                 = $keepalived::params::os_config,
-  $sysctl_config          = $keepalived::params::os_sysctl_config,
-  $sysctl_directives      = $keepalived::params::sysctl_directives,
-  $sysctl_config_template = $keepalived::params::os_sysctl_config_template,
-  $sysctl_reload_command  = $keepalived::params::os_sysctl_reload_command,
-  $global_definitions     = $keepalived::params::global_definitions,
-  $virtual_servers        = $keepalived::params::virtual_servers,
-  $vrrp_sync_groups       = $keepalived::params::vrrp_sync_groups,
-  $vrrp_instances         = $keepalived::params::vrrp_instances,
-  $vrrp_scripts           = $keepalived::params::vrrp_scripts,
-  $config_template        = $keepalived::params::os_config_template,
+  $repo                      = $keepalived::params::os_keepalived_repo,
+  $source                    = $keepalived::params::keepalived_source,
+  $revision                  = $keepalived::params::keepalived_revision,
+  $service                   = $keepalived::params::os_keepalived_service,
+  $service_ensure            = $keepalived::params::keepalived_service_ensure,
+  $dev_packages              = $keepalived::params::os_dev_packages,
+  $dev_ensure                = $keepalived::params::dev_ensure,
+  $build_options             = $keepalived::params::os_build_options,
+  $firewall_ports            = $keepalived::params::firewall_ports,
+  $config_dir                = $keepalived::params::os_config_dir,
+  $config_file               = $keepalived::params::os_config_file,
+  $sysctl_config             = $keepalived::params::os_sysctl_config,
+  $sysctl_directives         = $keepalived::params::sysctl_directives,
+  $sysctl_config_template    = $keepalived::params::os_sysctl_config_template,
+  $sysctl_reload_command     = $keepalived::params::os_sysctl_reload_command,
+  $build_package_init_script = $keepalived::params::os_build_package_init_script,
+  $init_script               = $keepalived::params::os_init_script,
+  $run_level_script          = $keepalived::params::os_run_level_script,
+  $global_definitions        = $keepalived::params::global_definitions,
+  $virtual_servers           = $keepalived::params::virtual_servers,
+  $vrrp_sync_groups          = $keepalived::params::vrrp_sync_groups,
+  $vrrp_instances            = $keepalived::params::vrrp_instances,
+  $vrrp_scripts              = $keepalived::params::vrrp_scripts,
+  $config_template           = $keepalived::params::os_config_template,
 
 ) inherits keepalived::params {
 
@@ -50,9 +57,12 @@ class keepalived (
   #-----------------------------------------------------------------------------
   # Installation
 
-  global::build { $repo:
-    source   => $source,
-    revision => $revision,
+  global::make { $repo:
+    source       => $source,
+    revision     => $revision,
+    dev_packages => $dev_packages,
+    dev_ensure   => $dev_ensure,
+    options      => $build_options,
   }
 
   #-----------------------------------------------------------------------------
@@ -64,10 +74,16 @@ class keepalived (
 
   #---
 
+  file { 'keepalived-config-dir':
+    path    => $config_dir,
+    ensure  => directory,
+    require => Global::Make[$repo],
+  }
+
   file { 'keepalived-config':
-    path    => $config,
+    path    => "${config_dir}/${config_file}",
     content => template($config_template),
-    require => Package['keepalived'],
+    require => File['keepalived-config-dir'],
     notify  => Service['keepalived'],
   }
 
@@ -80,9 +96,27 @@ class keepalived (
 
   exec { 'keepalived-sysctl-reload':
     command     => $sysctl_reload_command,
+    path        => [ '/sbin', '/usr/sbin', '/usr/local/sbin' ],
     refreshonly => true,
     require     => File['keepalived-sysctl-config'],
     notify      => Service['keepalived'],
+  }
+
+  if $init_script != $build_package_init_script {
+    file { 'keepalived-service':
+      ensure    => link,
+      path      => $init_script,
+      source    => $build_package_init_script,
+      subscribe => Global::Make[$repo],
+      notify    => Service['keepalived'],
+    }
+  }
+
+  file { "keepalived-run-level":
+    ensure    => link,
+    path      => $run_level_script,
+    source    => $build_package_init_script,
+    subscribe => Global::Make[$repo],
   }
 
   #-----------------------------------------------------------------------------
@@ -98,7 +132,7 @@ class keepalived (
   service { 'keepalived':
     name    => $service,
     ensure  => $service_ensure_real,
-    require => Package['keepalived'],
+    require => [ Global::Make[$repo], File['keepalived-sysctl-config'] ],
   }
 }
 
